d85354c4099a09151aa00526f5951aff146398ff
MONOO SINGH <monusikarwar2541@gmail.com>
2026-01-17 19:22:08 +0530

    changes surcharges

--- src/utils/csvImportHelper.ts
+++ src/utils/csvImportHelper.ts
@@ -70,14 +70,43 @@ export const prepareDataTariffs = (rows: any) => {
 
 export const prepareDataSurcharge = (rows: any) => {
   const Base: any = [];
-  console.log("prepareDataSurcharge: rows: ", rows);
+  // console.log("prepareDataSurcharge: rows: ", rows);
+ let headers:any = [];
 
-  const [header, ...dataRows] = rows;
+  let rowsWithoutHeadersIndex = 0;
+  for (let index = 0; index < rows.length; index++) {
+    const element = rows[index].toString().replace(/^\s*[\r\n]+/, '').split(",");
+   
+    if(element.includes("Term"))
+    {
+headers = [...element];
+break;
+    }
+     ++rowsWithoutHeadersIndex;
+  }
+if(headers?.length)
+{
+  
+   const termIndex = headers.findIndex((h:any) => h.trim().toLowerCase() == "term");
+   const valueIndex = headers.findIndex((h:any) => h.trim().toLowerCase() == "value");
+      const unitIndex = headers.findIndex((h:any) => h.trim().toLowerCase() == "unit");
+         const descriptionIndex = headers.findIndex((h:any) => h.trim().toLowerCase() == "description");
+            const typeIndex = headers.findIndex((h:any) => h.trim().toLowerCase() == "type");
+              for (let index = rowsWithoutHeadersIndex; index < rows.length; index++) {
+    const element = rows[index].toString().replace(/^\s*[\r\n]+/, '').split(",");
+   
+    const objectValue = {
+            "id":uuidv4(),
+            "Term":termIndex>=0?element[termIndex]:"",
+            "Value":valueIndex>=0?element[valueIndex]:"",
+            "Unit":unitIndex>=0?element[unitIndex]:"",
+            "Description":descriptionIndex>=0?element[descriptionIndex]:"",
+            Type:typeIndex>=0?element[typeIndex]:"",
+          }
+Base.push(objectValue);
 
-  console.log("prepareDataSurcharge: header: ", header);
-  console.log("prepareDataSurcharge: dataRows: ", dataRows);
-  console.log("prepareDataSurcharge: dataRows: ", dataRows);
-  header.map((item: any, index: any) => {});
-
-  return Base;
+  }
+}
+  
+ return Base;
 };
